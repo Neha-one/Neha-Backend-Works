f1ad9257f4902eb2637deb6872c997c47d068225
Neha-one <justforgtb@gmail.com>
2026-08-29 16:47:08 +0530

    add these files

---
+++ StayNest-with-db/routes/store.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const storeRouter = express.Router();
+const storeController = require('../controller/storeController');
+
+storeRouter.get("/", storeController.getIndex)
+storeRouter.get("/home", storeController.getHome)
+storeRouter.get('/home/:homeId', storeController.getHomeDetails);
+storeRouter.get("/booking", storeController.getBooking)
+storeRouter.get("/favorite", storeController.getFavorite)
+storeRouter.post("/favorite", storeController.postAddFavorite)
+
+module.exports = { storeRouter };
